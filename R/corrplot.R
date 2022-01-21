3936eef4ff918cf4c30e1fecfbe4b5ca5656c81c
ruben1dlg <ruben1dlg@gmail.com>
2022-01-20 18:00:59 -0800

    Added first function, test folder and updated DESCRIPTION

--- R/corrplot.R
+++ R/corrplot.R
@@ -1,2 +1,13 @@
+#' Create a correlation plot
+#'
+#' @param df A dataframe to use for the correlation plot.
+#' @param print_corr Whether or not to print the correlation values between variables
+#' @param shape Shape to use in the plot
+#'
+#' @return A ggplot object.
+#' @export
+#'
+#' @examples
+#' corrplot(df, print_corr = FALSE, shape = "square")
 corrplot <- function(df, print_corr=FALSE, shape="circle") {
 }
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(magmavizR)
+
+test_check("magmavizR")
