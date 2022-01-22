636832cbba72ad17dddbd0969a3b78662b5bae7c
Mukund Iyer <msiyer@student.ubc.ca>
2022-01-22 02:24:21 -0800

    feat: Create shell for boxplot function with docstring

---
+++ R/boxplot.R
@@ -0,0 +1,14 @@
+#' Plot a boxplot with the magma color scheme and an option to facet.
+#'
+#' @param df Dataframe containing the variables for plotting
+#' @param x Column name of the numerical variable to view the distribution of
+#' @param y Column name containing the categorical variables to assign boxes to
+#' @param facet Determines whether separate graphs will be created for each category
+#'
+#' @return a ggplot object
+#' @export
+#'
+#' @examples
+#' function(df, "Length", "Species", TRUE)
+corrplot <- function(df, x, y, facet = FALSE) {
+}
