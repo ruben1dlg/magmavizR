3a0f72e10639d1ade3c949ea2328ee1d9012549e
Mukund Iyer <msiyer@student.ubc.ca>
2022-01-22 01:55:33 -0800

    feat: created shell for boxplot function

---
+++ R/boxplot.R
@@ -0,0 +1,14 @@
+#' Create a boxplot with magma theme
+#'
+#' @param df Dataframe containing the variables for plotting
+#' @param x Column name of the numerical variable to view the distribution of
+#' @param y Column name containing the categorical variables to assign boxes to
+#' @param facet Determines whether separate graphs will be created for each category
+#'
+#' @return A ggplot object
+#' @export
+#'
+#' @examples'
+#' boxplot(df, Miles_per_gallon, Country_of_origin, TRUE)
+boxplot <- function(df, x, y, facet=FALSE){
+}
