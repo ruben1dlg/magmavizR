9c8d7a9987e6d128a090673c7e4506d7295ff711
Mukund Iyer <msiyer@student.ubc.ca>
2022-01-24 18:14:14 -0800

    test: added test for dataframe type

--- tests/testthat/test-boxplot.R
+++ tests/testthat/test-boxplot.R
@@ -18,39 +18,39 @@ df <- tibble('miles_per_gallon' = x,
 
 plot <- boxplot(df,miles_per_gallon,origin,facet=TRUE)
 
-test_that("The check for input type for x is failing",{
-    expect_error(boxplot(df,"miles_per_gallon",origin))
-})
 
 test_that("The check for input type for x is failing",{
-    expect_error(boxplot(df,miles_per_gallon,"origin"))
+    expect_error(boxplot(df,"miles_per_gallon",origin),
+                 "x, the column name for x, must be unquoted")
+})
+
+
+test_that("The check for input type for y is failing",{
+    expect_error(boxplot(df,miles_per_gallon,"origin",
+                    "y, the column name of y, must be unquoted"))
+})
+
+
+test_that("The check for input type for df is failing",{
+    expect_error(boxplot(c(1,2),miles_per_gallon,origin,
+                         "y, the column name of y, must be unquoted"))
+})
+
+
+
+test_that("The check for column name for x is failing",{
+    expect_error(boxplot(df,miles_per_gallo,origin),
+    'x column name not found')
+})
+
+
+test_that("The check for column name for y is failing",{
+    expect_error(boxplot(df,miles_per_gallon,origi),
+                 'y column name not found')
 
 })
 
-test_that("The check for input type for x is failing",{
-    expect_error(boxplot(df,miles_per_gallo,origin))
-
-})
-
-test_that("The check for input type for x is failing",{
-    expect_error(boxplot(df,miles_per_gallon,origi))
-
-})
-
-#test_that("Column name for x must exist in df",{
-#    expect_error(boxplot(df,miles_per_gallo,origin))
-
-#})
-
-#check input types
 
 
-#test_that('Plot should map x to x-axis and y to y-axis.', {
-#    expect_true("miles_per_gallon"  == rlang::get_expr(plot2d$mapping$x))
-#    expect_true("origin" == rlang::get_expr(plot2d$mapping$y))
-#})
 
 
-#expect_error()
-
-#expect_is()
