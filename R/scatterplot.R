24344fdcc755046dcffe3faf1eb8256b8f08e0eb
Abdul Moid Mohammed <ibaad90@gmail.com>
2022-01-26 21:15:39 -0800

    feat: add x, y axes and color legend titles based on columns if not assigned, customize plot title if not assigned and associated tests

--- R/scatterplot.R
+++ R/scatterplot.R
@@ -36,13 +36,13 @@ library(rlang)
 #'                 "Iris Sepal Length vs Sepal Width across Species",
 #'                 1.0, 50, "Sepal Length", "Sepal Width", "", False, False, True)
 
-scatterplot <- function(df, x, y, c=NULL, t="", o=0.5, s=5) {
+scatterplot <- function(df, x, y, c=NULL, t="", o=0.5, s=5, xtitle="", ytitle="", ctitle="") {
 
     base::missing(c)
 
     # check to ensure df is assigned a dataframe
     if (!is.data.frame(df) == TRUE) {
-        stop("Data assigned to 'df' must be a dataframe")
+        stop("Data assigned to 'df' must be a dataframe.")
     }
 
     # check to ensure x is not in quotes
@@ -50,7 +50,7 @@ scatterplot <- function(df, x, y, c=NULL, t="", o=0.5, s=5) {
         typeof(substitute(x)) !=
         typeof(as.list(quote(symbol))[[1]])
         ) {
-        stop("Column name assigned to 'x' must not be in quotes")
+        stop("Column name assigned to 'x' must not be in quotes.")
     }
 
     # check to ensure y is not in quotes
@@ -58,64 +58,87 @@ scatterplot <- function(df, x, y, c=NULL, t="", o=0.5, s=5) {
         typeof(substitute(y)) !=
         typeof(as.list(quote(symbol))[[1]])
     ) {
-        stop("Column name assigned to 'y' must not be in quotes")
+        stop("Column name assigned to 'y' must not be in quotes.")
     }
 
     # check if column x is present in the df
     if (!as.character(ggplot2::vars({{ x }})[[1]])[2] %in% colnames(df)) {
-        stop("Column assigned to 'x' not found in dataframe")
+        stop("Column assigned to 'x' not found in dataframe.")
     }
 
     # check if column y is present in the df
     if (!as.character(ggplot2::vars({{ y }})[[1]])[2] %in% colnames(df)) {
-        stop("Column assigned to 'y' not found in dataframe")
+        stop("Column assigned to 'y' not found in dataframe.")
     }
 
     # check if column has been assigned to color c
-    if (is.null(substitute(c)) == TRUE) {
-        # if c is NULL (default) keep only x and y in title if title is blank
-        if (t == "") {
-            t <- paste(
-                str_to_sentence(str_replace_all(as.character(ggplot2::vars({{ x }})[[1]])[2], "[_!.]", " ")),
-                "vs",
-                str_to_sentence(str_replace_all(as.character(ggplot2::vars({{ y }})[[1]])[2], "[_!.]", " ")),
-                sep = " "
-            )
-        }
-    # checks for when a column is assigned to c
-    } else {
+    if (!is.null(substitute(c)) == TRUE) {
         # check to ensure c is not in quotes
         if (
             (typeof(rlang::get_expr(ggplot2::vars({{ c }})[[1]])) !=
              typeof(as.list(quote(symbol))[[1]]))
         ) {
-            stop("Column name assigned to 'color' must not be in quotes")
+            stop("Column name assigned to 'color' must not be in quotes.")
         }
 
         # check if column c is present in the df
         if (!as.character(ggplot2::vars({{ c }})[[1]])[2] %in% colnames(df)) {
-            stop("Column assigned to 'color' not found in dataframe")
+            stop("Column assigned to 'color' not found in dataframe.")
         }
+    }
 
-        # add color variable to title along with x and y if title is blank
-        if (t == "") {
-            t <- paste(
-                str_to_sentence(str_replace_all(as.character(ggplot2::vars({{ x }})[[1]])[2], "[_!.]", " ")),
-                "vs",
-                str_to_sentence(str_replace_all(as.character(ggplot2::vars({{ y }})[[1]])[2], "[_!.]", " ")),
-                "by",
-                str_to_sentence(str_replace_all(as.character(ggplot2::vars({{ c }})[[1]])[2], "[_!.]", " ")),
-                sep = " "
-            )
-        }
+    # check if title is a string
+    if (!typeof(substitute(t)) == 'character') {
+        stop("Title must be assigned in quotes as a string.")
     }
 
     if (o < 0.1 | o > 1.0) {
-        stop("Opacity (alpha) must be between 0.1 and 1.0 (inclusive)")
+        stop("Opacity (alpha) must be between 0.1 and 1.0 (inclusive).")
     }
 
     if (s < 1 | s > 5) {
-        stop("Size must be between 1 and 5 (inclusive)")
+        stop("Size must be between 1 and 5 (inclusive).")
+    }
+
+    # check if x axis title is a string
+    if (!typeof(substitute(xtitle)) == 'character') {
+        stop("Title assigned to x-axis must be a string in quotes.")
+    }
+
+    # check if y axis title is a string
+    if (!typeof(substitute(ytitle)) == 'character') {
+        stop("Title assigned to y-axis must be a string in quotes.")
+    }
+
+    # check if color title is a string
+    if (!typeof(substitute(ctitle)) == 'character') {
+        stop("Title assigned to color legend must be a string in quotes.")
+    }
+
+    # renaming x axis if custom x axis title not assigned
+    if(xtitle == "") {
+        xtitle <- str_to_sentence(str_replace_all(as.character(ggplot2::vars({{ x }})[[1]])[2], "[_!.]", " "))
+    }
+
+    # renaming y axis if custom x axis title not assigned
+    if(ytitle == "") {
+        ytitle <- str_to_sentence(str_replace_all(as.character(ggplot2::vars({{ y }})[[1]])[2], "[_!.]", " "))
+    }
+
+    # renaming x axis if custom x axis title not assigned
+    if(ctitle == "") {
+        ctitle <- str_to_sentence(str_replace_all(as.character(ggplot2::vars({{ c }})[[1]])[2], "[_!.]", " "))
+    }
+
+    # defining title based on x, y and/or c names if no title assigned
+    if(t == "") {
+        # if c is NULL (default) keep only x and y in title if title is blank
+        if (is.null(substitute(c)) == TRUE) {
+            t <- paste(xtitle, "vs", ytitle, sep = " ")
+        } else {
+        # add color variable to title along with x and y if title is blank
+            t <- paste(xtitle, "vs", ytitle, "by", ctitle, sep = " ")
+        }
     }
 
     splot <- ggplot2::ggplot(df,
@@ -133,7 +156,10 @@ scatterplot <- function(df, x, y, c=NULL, t="", o=0.5, s=5) {
             option = "magma"
         ) +
         labs(
-            title = t
+            title = t,
+            x = xtitle,
+            y = ytitle,
+            color = ctitle
         )
 
 return(splot)
