af4f684f63cc14e7909c0355176b6a42d49586f7
Abdul Moid Mohammed <ibaad90@gmail.com>
2022-01-27 00:54:16 -0800

    feat: added advanced scatterplot function call with many parameters and associated tests, added comments

--- tests/testthat/test-scatterplot.R
+++ tests/testthat/test-scatterplot.R
@@ -14,17 +14,40 @@ sample_data <- dplyr::tibble(
                      10083831, 7988479, 9872424)
 )
 
+# basic scatterplot with df, x and y axis
 scatter_basic <- scatterplot(sample_data, year, no_of_cars)
 
-
+# test_that for basic scatterplot - expected inputs
 test_that('Scatterplot (basic) should use geom_point and map x to x-axis, and y to y-axis.', {
     expect_true("GeomPoint" %in% c(class(scatter_basic$layers[[1]]$geom)))
     expect_true("year"  == rlang::get_expr(scatter_basic$mapping$x))
     expect_true("no_of_cars" == rlang::get_expr(scatter_basic$mapping$y))
 })
 
+# test_that for basic scatterplot - default not expected inputs
 test_that('Scatterplot (basic) must have default values for all parameters except df, x, and y.', {
     expect_null(rlang::get_expr(scatter_basic$mapping$c))
     expect_true(0.5 == rlang::get_expr(scatter_basic$layers[[1]]$aes_params$alpha))
     expect_true(3 == rlang::get_expr(scatter_basic$layers[[1]]$aes_params$size))
+})
+
+# advanced scatterplot with all parameters
+scatter_advanced <- scatterplot(sample_data, year, no_of_cars, brand,
+                                "Total cars produced from 2015 to 2017 by Car brands",
+                                0.6, 4, "", "Total Cars", "Car Brand", FALSE, FALSE, TRUE)
+
+# test_that for advanced scatterplot - expected inputs
+test_that('Scatterplot (basic) should use geom_point and map x to x-axis, and y to y-axis.', {
+    expect_true("GeomPoint" %in% c(class(scatter_advanced$layers[[1]]$geom)))
+    expect_true("year"  == rlang::get_expr(scatter_advanced$mapping$x))
+    expect_true("no_of_cars" == rlang::get_expr(scatter_advanced$mapping$y))
+    expect_true("brand" == rlang::get_expr(scatter_advanced$mapping$colour))
+    expect_true("brand" == rlang::get_expr(scatter_advanced$mapping$shape))
+    expect_true("Total cars produced from 2015 to 2017 by Car brands" == rlang::get_expr(scatter_advanced$labels$title))
+    expect_true("Year" == rlang::get_expr(scatter_advanced$labels$x))
+    expect_true("Total Cars" == rlang::get_expr(scatter_advanced$labels$y))
+    expect_true("Car Brand" == rlang::get_expr(scatter_advanced$labels$colour))
+    expect_true("Car Brand" == rlang::get_expr(scatter_advanced$labels$shape))
+    expect_true(0.6 == rlang::get_expr(scatter_advanced$layers[[1]]$aes_params$alpha))
+    expect_true(4 == rlang::get_expr(scatter_advanced$layers[[1]]$aes_params$size))
 })
