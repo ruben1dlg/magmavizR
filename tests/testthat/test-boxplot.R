adbd2b64b4bb6f583b28acae5037b8326aa8c95d
Mukund Iyer <msiyer@student.ubc.ca>
2022-01-28 13:04:05 -0800

    test: added 2 tests to check data type errors

--- tests/testthat/test-boxplot.R
+++ tests/testthat/test-boxplot.R
@@ -14,42 +14,52 @@ y <- c("USA",
        "USA",
        "Europe")
 
-df <- dplyr::tibble('miles_per_gallon' = x,
-             'origin' = y)
+df <- dplyr::tibble('miles_per_gallon' = y,
+                    'origin' = x)
 
-plot <- boxplot(df,miles_per_gallon,origin)
-plot_facet <- boxplot(df,miles_per_gallon,origin, facet=TRUE)
+plot <- boxplot(df, origin, miles_per_gallon)
+plot_facet <- boxplot(df, origin, miles_per_gallon, facet=TRUE)
 
 
 test_that("The check for input type for x is failing",{
-    expect_error(boxplot(df,"miles_per_gallon",origin),
+    expect_error(boxplot(df, "origin", miles_per_gallon),
                  "x, the column name for x, must be unquoted")
 })
 
 
 test_that("The check for input type for y is failing",{
-    expect_error(boxplot(df,miles_per_gallon,"origin",
+    expect_error(boxplot(df, origin, "miles_per_gallon",
                          "y, the column name of y, must be unquoted"))
 })
 
 
 test_that("The check for input type for df is failing",{
-    expect_error(boxplot(c(1,2),miles_per_gallon,origin,
+    expect_error(boxplot(c(1,2), miles_per_gallon, origin,
                          "y, the column name of y, must be unquoted"))
 })
 
-
-
 test_that("The check for column name for x is failing",{
-    expect_error(boxplot(df,miles_per_gallo,origin),
+    expect_error(boxplot(df,origin,miles_per_gallon),
                  'x column name not found')
 })
 
 
 test_that("The check for column name for y is failing",{
-    expect_error(boxplot(df,miles_per_gallon,origi),
+    expect_error(boxplot(df,origin,miles_per_gallon),
                  'y column name not found')
 
+})
+
+test_that("Test for the type of x is failing", {
+
+    expect_error(boxplot(df,miles_per_gallon, origin),
+                 'x needs to be a factor or character')
+})
+
+test_that("Test for the type of y is failing", {
+
+    expect_error(boxplot(df,origin, origin),
+                 'y needs to be numeric')
 })
 
 ###output tests
