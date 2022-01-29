6325f91dbbe9475db6b765ed6fa6b07b2fce75a1
Mukund Iyer <msiyer@student.ubc.ca>
2022-01-28 16:25:31 -0800

    feat: fixed indent issue

--- R/boxplot.R
+++ R/boxplot.R
@@ -14,80 +14,80 @@
 #'
 boxplot <- function(df, x, y, facet = FALSE){
 
-    library(viridis)
-    library(stringr)
-    library(rlang)
-    library(ggplot2)
-    library(dplyr)
+library(viridis)
+library(stringr)
+library(rlang)
+library(ggplot2)
+library(dplyr)
 
-    ### input tests
-    #check type of x
-    if ( typeof(rlang::get_expr(ggplot2::vars({{ x }})[[1]])) !=
+### input tests
+#check type of x
+if ( typeof(rlang::get_expr(ggplot2::vars({{ x }})[[1]])) !=
          typeof(as.list(quote(symbol))[[1]])){
         stop("x, the column name for x, must be unquoted")
-    }
+}
 
-    #check type of y
-    if ( typeof(rlang::get_expr(ggplot2::vars({{ y }})[[1]])) !=
+#check type of y
+if ( typeof(rlang::get_expr(ggplot2::vars({{ y }})[[1]])) !=
          typeof(as.list(quote(symbol))[[1]])){
         stop("y, the column name of y, must be unquoted")
-    }
+}
 
     #check type of df
-    if ( typeof(df) != 'list' ){
+if ( typeof(df) != 'list' ){
         stop("df must be a dataframe")
-    }
+}
 
     #check if x exists in df
-    if ( !(as.character(as.list(
+if ( !(as.character(as.list(
         rlang::get_expr(ggplot2::vars({{ x }})[[1]])
     )))
     %in%
     colnames({{ df }})
     ){
         stop('x column name not found')
-    }
+}
 
     #check if y exists in df
-    if ( !(as.character(as.list(
+if ( !(as.character(as.list(
         rlang::get_expr(ggplot2::vars({{ y }})[[1]])
     )))
     %in%
     colnames({{ df }})
     ){
         stop('y column name not found')
-    }
+}
 
     #check the type for x
-    if(!(
+if(!(
         is.character((df |> dplyr::select({{ x }}))[[1]]) |
         is.factor((df |> dplyr::select({{ x }}))[[1]]))
     ){
         stop('x needs to have distinct categories')
-    }
+}
 
     #check the type for y
-    if(!(
+if(!(
         is.numeric((df |> dplyr::select({{ y }}))[[1]])
     )){
         stop('y needs to be numeric')
-    }
+}
 
 
 
-    x_title <- stringr::str_replace(as.character
+x_title <- stringr::str_replace(as.character
                                     (as.list(
                                         rlang::get_expr(ggplot2::vars({{ x }})[[1]]))),
                                     "[_!.]",
                                     " ")
 
-    y_title <- stringr::str_replace(as.character
+y_title <- stringr::str_replace(as.character
                                     (as.list(
                                         rlang::get_expr(ggplot2::vars({{ y }})[[1]]))),
                                     "[_!.]",
                                     " ")
 
-    plot <- ggplot2::ggplot(df) +
+plot <- ggplot2::ggplot(df) +
         geom_boxplot(aes(
             x={{ x }},
             y={{ y }},
@@ -99,7 +99,7 @@ boxplot <- function(df, x, y, facet = FALSE){
         ) +
         theme(legend.position = "none")
 
-    if (facet == TRUE){
+if (facet == TRUE){
         return(plot  +
                    ggplot2::facet_wrap(ggplot2::vars({{ x }}))
         )
